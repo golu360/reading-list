dd2d2d42c81e461079863126936eb7b54e4a38f8
Abhishek Mishra <abhishek.mishra@nium.com>
2024-03-16 02:09:17 +0530

    New scaffolding

---
+++ app/components/header.tsx
@@ -0,0 +1,40 @@
+import { LaptopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Link } from "@remix-run/react";
+import * as React from "react";
+import { useHydrated } from "remix-utils/use-hydrated";
+
+import {
+	getTheme,
+	setTheme as setSystemTheme,
+} from "@/components/theme-switcher";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import HomeIcon from "../images/icon.png";
+
+export function Header() {
+	const hydrated = useHydrated();
+	const [, rerender] = React.useState({});
+	const setTheme = React.useCallback((theme: string) => {
+		setSystemTheme("dark");
+		rerender({});
+	}, []);
+	const theme = getTheme();
+
+	return (
+		<header className="flex items-center justify-between px-4 py-2 md:py-4">
+			<div className="flex items-center space-x-4">
+				<Link className="flex items-center space-x-2" to="/">
+					{/* <HomeIcon className="h-6 w-6" /> */}
+					<img src={HomeIcon}className="text-lg font-bold" width="64" height="64"/>
+				</Link>
+			</div>
+		</header>
+	);
+}
